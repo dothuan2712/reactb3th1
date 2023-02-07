48ac4d80cd63ff4cf15b730619c4fd0b33de6751
dothuan2712 <dothuan2712@gmail.com>
2023-02-07 20:28:31 +0700

    revise image size

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+
 import th1photo from './th1photo.jpg';
 import { base64Photo } from './Constant';
 import logo from './logo.svg';
@@ -9,7 +10,7 @@ export default function App() {
   return (
     <div className="container">
       <div> Đây là hướng dẫn dùng ảnh trong reactjs</div>
-        <img src={th1photo} width="200" height='100' className ='img'></img>
+        <img src={th1photo} width="512" height='340' className ='img'></img>
         <div className='require-title'>Đây là ảnh dùng require</div>
         <img src={require('./th1photo.jpg')} className='img2'></img>
         <div className='link'>Đây là ảnh dùng link online</div>
